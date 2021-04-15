24496655d9066fdb43ba69632b890cc08a98ac27
Zuzana Belaskova <z.belaskova@gmail.com>
2021-04-15 19:29:05 +0200

    onClick in Calculator component changed to onSubmit

--- src/components/Calculator/index.jsx
+++ src/components/Calculator/index.jsx
@@ -9,47 +9,60 @@ export const Calculator = () => {
   const [pricePerYear, setPricePerYear] = useState('');
   const [submitted, setSubmitted] = useState(false);
 
-  const handleClick = () => {
-    setSubmitted(true);
+  const count = () => {
     setPricePerDay((packageCost / 20) * cigarettesPerDay);
     setPricePerMonth(pricePerDay * 30);
     setPricePerYear(pricePerMonth * 12);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    count();
+    console.log(
+      `za den ${pricePerDay}, za měsíc ${pricePerMonth}, za rok ${pricePerYear}`,
+    );
+    setSubmitted(true);
+  };
+
   return (
     <>
-      <label>
-        Kolik cigaret denně vykouříte?
-        <input
-          type="text"
-          value={cigarettesPerDay}
-          onChange={(e) => setCigarettesPerDay(e.target.value)}
-        />
-      </label>
-      <label>
-        Kolik Vás stojí krabička cigaret?
-        <input
-          type="text"
-          value={packageCost}
-          onChange={(e) => setPackageCost(e.target.value)}
-        />
-      </label>
-      <button
-        type="submit"
-        onClick={handleClick}
-        disabled={cigarettesPerDay === '' && packageCost === ''}
-      >
-        Vypočítat
-      </button>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Kolik cigaret denně vykouříte?
+          <input
+            type="text"
+            value={cigarettesPerDay}
+            onChange={(e) => setCigarettesPerDay(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Kolik Vás stojí krabička cigaret?
+          <input
+            type="text"
+            value={packageCost}
+            onChange={(e) => setPackageCost(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <button
+          type="submit"
+          disabled={cigarettesPerDay === '' || packageCost === ''}
+        >
+          Vypočítat
+        </button>
 
-      <div className="result">
-        {' '}
-        {submitted
-          ? `Kouření Vás průměrně stojí: `
-          : 'Pro výpočet vyplňte požadovaná pole.'}
-        <div>{submitted ? `${pricePerMonth} korun měsíčně.` : null}</div>
-        <div>{submitted ? `${pricePerYear} korun ročně.` : null}</div>
-      </div>
+        <div className="result">
+          {' '}
+          {submitted
+            ? `Kouření Vás průměrně stojí: `
+            : 'Pro výpočet vyplňte požadovaná pole.'}
+          <div>{submitted ? `${pricePerMonth} korun měsíčně.` : null}</div>
+          <div>{submitted ? `${pricePerYear} korun ročně.` : null}</div>
+        </div>
+      </form>
     </>
   );
 };
